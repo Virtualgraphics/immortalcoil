f1fa060a763447a855b6ce65e0d8da09bd9bed43
David Boller <david@virtual-graphics.ch>
2024-05-24 16:31:26 +0200

    minor update

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = "2796223b1c7e6ca8d6b999a00dce9e99";
 
 if (!clientId) {
   throw new Error("No client ID provided");
